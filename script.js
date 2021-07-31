308ea76a225cf3b19ece6357330975fc5967876c
PandaXD123 <68870750+PandaXD123@users.noreply.github.com>
2021-07-31 19:31:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -27,3 +27,54 @@ $(function() {
             }
     });
 });
+
+/*
+var darkmode= true;
+$(function() {
+    $(".btn").click(function()
+    {
+        if(darkmode == true)
+        darkmode = false;
+        else
+        darkmode = true;
+        var element = document.body;
+        element.classList.toggle("darkmodeBody");
+
+        if(darkmode == false)
+        {
+            $("div[class='feature']").css({
+                'color': 'black',
+                'text-shadow':'1px 1px 1px white'
+            });
+            $("h2").css({
+                'color': 'black',
+                'text-shadow':'1px 1px 1px white'
+            });
+            $("section[class='quote']").css({
+                'color': 'black'
+            });
+            $("cite").css({
+                'color': 'black'
+            });
+            $("div[id='navbar']").css ({
+                'background-image': 'linear-gradient(to right, #ebebeb, #d8d8d8, #ebebeb)'
+            });
+            $("a").css ({
+                'color': 'black',
+                'text-shadow': '1px 1px 1px white'
+            });
+        }
+        else
+        {
+            $("div[class='feature']").css({
+                'color': 'white',
+                'text-shadow':'1px 1px 1px black'
+            });
+            $("h2").css({
+                'color': 'white',
+                'text-shadow':'1px 1px 1px black'
+            });      
+        }                
+    });
+});
+*/
